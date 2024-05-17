c49d5d08c6b7d97ab675c3ccdd199231c55054a7
willing <87075578+gb16001@users.noreply.github.com>
2024-05-17 11:18:33 +0800

    fix args bug

--- test_LPRNet.py
+++ test_LPRNet.py
@@ -28,13 +28,12 @@ def get_parser():
     parser.add_argument('--test_img_dirs', default="./data/test", help='the test images path')
     parser.add_argument('--dropout_rate', default=0, help='dropout rate.')
     parser.add_argument('--lpr_max_len', default=8, help='license plate number max length.')
-    parser.add_argument('--test_batch_size', default=100, help='testing batch size.')
+    parser.add_argument('--test_batch_size', default=100, type=int, help='testing batch size.')
     parser.add_argument('--phase_train', default=False, type=bool, help='train or test phase flag.')
     parser.add_argument('--num_workers', default=8, type=int, help='Number of workers used in dataloading')
     parser.add_argument('--cuda', default=True, type=bool, help='Use cuda to train model')
-    parser.add_argument('--show', default=False, type=bool, help='show test image and its predict result or not.')
+    parser.add_argument('--show', default=False, type=bool, action='store_true',help='show test image and its predict result or not.')
     parser.add_argument('--pretrained_model', default='./weights/Final_LPRNet_model.pth', help='pretrained base model')
-
     args = parser.parse_args()
 
     return args
